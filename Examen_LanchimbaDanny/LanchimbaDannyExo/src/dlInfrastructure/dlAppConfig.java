8fbf1c31dbfc50b2a8266d74f15864fe7454563d
023daniii <agb.dan.zzz@gmail.com>
2026-02-09 22:18:07 -0600

    Delete Examen_LanchimbaDanny directory

--- Examen_LanchimbaDanny/LanchimbaDannyExo/src/dlInfrastructure/dlAppConfig.java
+++
@@ -1,7 +0,0 @@
-package dlInfrastructure;
-
-public class dlAppConfig {
-    // Asegúrate de que los nombres de carpetas coincidan con lo que creaste
-    public static final String DL_LOG_FILE = "storage/DataFiles/ExoTracer.txt";
-    public static final String DL_DATA_FILE = "storage/DataFiles/ExoMunision.txt";
-}
